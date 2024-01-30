938126345c478d0ccba5c814e20a59fe8cb0a6dc
abhi111456 <abhishek.21jics039@jietjodhpur.ac.in>
2024-01-30 16:47:56 +0530

    a

--- src/components/LoginAndLogout.js
+++ src/components/LoginAndLogout.js
@@ -15,9 +15,8 @@ export default function LoginAndLogout() {
     // Signed up 
     const user = userCredential.user;
     console.log(user);
-    console.log('Name:', name);
-    console.log('Email:', email);
-    console.log('Password:', pass);
+    
+    
     // ...
   })
   .catch((error) => {
@@ -25,6 +24,7 @@ export default function LoginAndLogout() {
     const errorMessage = error.message;
     // ..
   });
+ 
 
 
  }
